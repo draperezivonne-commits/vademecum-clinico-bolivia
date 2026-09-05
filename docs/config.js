179ba62e0afeba54016a9f81aaecbe93de9b5114
draperezivonne-commits <dra.perezivonne@gmail.com>
2026-09-05 19:30:09 -0400

    v0.19.2 actualizar versión PWA

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.VCB_CONFIG={
-  appVersion:'0.19.1',
+  appVersion:'0.19.2',
   bundledCatalogVersion:'0.19.0',
   updateManifestUrl:'./data/catalog-version.json',
   syncIntervalMs:86400000,

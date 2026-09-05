7949939aeec0faadfdcf70dec5af06f203a6b915
draperezivonne-commits <dra.perezivonne@gmail.com>
2026-09-05 19:09:40 -0400

    v0.19.1 identidad visual oficial e icono PWA

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.VCB_CONFIG={
-  appVersion:'0.19.0',
+  appVersion:'0.19.1',
   bundledCatalogVersion:'0.19.0',
   updateManifestUrl:'./data/catalog-version.json',
   syncIntervalMs:86400000,
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='vcb-shell-0.19.0';
+const CACHE_NAME='vcb-shell-0.19.1';
 const PRECACHE=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./data/seed.js','./data/supplies.js','./data/spine_miss.js','./data/spine_approaches.js','./data/pediatric_guides.js','./data/interactions.js','./data/regulatory_exclusions.js','./data/regulatory_alerts.js','./data/catalog-version.json','./data/catalogo_medicamentos_v0.19.0.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});

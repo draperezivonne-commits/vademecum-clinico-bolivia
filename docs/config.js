66caaf6f8ea3c743445db34cb36f1ff04ebbb1a1
draperezivonne-commits <dra.perezivonne@gmail.com>
2026-09-05 20:04:46 -0400

    v0.19.3 actualizar configuración de aplicación

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.VCB_CONFIG={
-  appVersion:'0.19.2',
+  appVersion:'0.19.3',
   bundledCatalogVersion:'0.19.0',
   updateManifestUrl:'./data/catalog-version.json',
   syncIntervalMs:86400000,
